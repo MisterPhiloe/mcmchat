d7c885349382809781b1354d3ef42c3ff2d0c8f8
Philippe van Mastrigt <philippe@vanmastrigt.eu>
2016-12-17 00:55:42 +0100

    Added Vault as dependency

--- src/main/java/com/morecommunityminecraft/mcmchat/Main.java
+++ src/main/java/com/morecommunityminecraft/mcmchat/Main.java
@@ -1,16 +1,48 @@
 package com.morecommunityminecraft.mcmchat;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin {
 
+    private static Main main;
+    private final Logger log = Logger.getLogger("Minecraft");
+    private Permission perms = null;
+    private Chat chat = null;
+
+    public static Main getInstance() {
+        return main;
+    }
+
     @Override
     public void onEnable() {
-
+        main = this;
+        log.info(getName() + " has been enabled!");
+        setupPermissions();
+        setupChat();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        log.info(getName() + " has been disabled!");
+        main = null;
     }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
 }
---
+++ src/main/java/com/morecommunityminecraft/mcmchat/events/ChatEvent.java
@@ -0,0 +1,15 @@
+package com.morecommunityminecraft.mcmchat.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatEvent implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        Player player = e.getPlayer();
+
+    }
+}
